319fccb6cd020f11ae420b03e8046ecb900c2084
gwatamalou <gwatir@yandex.ru>
2024-07-18 21:42:38 +0600

    init

---
+++ module_9_6.py
@@ -0,0 +1,10 @@
+def all_variants(text: str):
+    for i in range(1, len(text)+1):
+        j = 0
+        while j+i <= len(text):
+            yield text[j:j+i]
+            j+=1
+
+a = all_variants("abc")
+for i in a:
+    print(i)
