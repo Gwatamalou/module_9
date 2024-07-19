e9b47264e0217d4e5ef51ebe55dec7fc369a6acf
gwatamalou <gwatir@yandex.ru>
2024-07-19 21:49:54 +0600

    init

--- module_9_7.py
+++ module_9_7.py
@@ -1,10 +1,18 @@
 
 def is_prime(func):
-    def wrapper():
-        n = func()
+    def wrapper(*number):
+        n = func(*number)
         dn = 2
         while dn*dn <= n and n % dn == 0: dn += 1
-        return n, dn
+        print("Составное") if dn*dn > n else print("Простое")
+        return n
     return wrapper
-def sum_three(*number: int) -> int:
+
+
+@is_prime
+def sum_three(*number):
     return sum(number)
+
+
+result = sum_three(2, 3, 6)
+print(result)
